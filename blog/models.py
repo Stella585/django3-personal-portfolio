a4c00a9e4eeb28a6526e0bc6a8b9982114afeeea
Суворова Стелла Викторовна <55.SuvorovaSV@rosstat.local>
2024-08-06 14:14:17 +0600

    First commit!

---
+++ blog/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from datetime import date
+
+
+class Blog(models.Model):
+    title=models.CharField(max_length=200)
+    description=models.TextField()
+    date = models.DateField(default=date.today)
+    def __str__(self):
+        return self.title
+      
+   
+
+
